3f79c06992320dabb1cd964ae99254cd1edaa840
Stephen Reish <spreish@ncsu.edu>
2022-05-04 16:02:29 -0400

    Attempting to display base page

--- albums/models.py
+++ albums/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(null=True, blank=True, max_length=255)
+    artist = models.CharField(null=True, blank=True, max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Album
+from .forms import AlbumForm
+
 
 # Create your views here.
+def list_albums(request):
+    albums = Album.objects.all()
+    return render(request, "",
+                {"albums": albums})
